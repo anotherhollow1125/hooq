fed85825dedf6bbb1e4f1983cd6197096732ed26
namnium <anotherhollow1125@gmail.com>
2025-08-05 03:45:12 +0900

    Revert "wip: 一旦rustfmt::skipを付与してみる"
    This reverts commit 7fbb0cfb3cc4025d64316d24fd8aee426fc4658d.

--- src/impls/option/mod.rs
+++ src/impls/option/mod.rs
@@ -15,7 +15,6 @@ pub struct HooqOption {
 fn default_method() -> TokenStream {
     parse_quote! {
         .inspect_err(|e| {
-            #[rustfmt::skip]
             ::std::eprintln!("{:?} @ path: {}, line: {}", e, $path, $line);
         })
     }

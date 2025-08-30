b9ad7d60412f0c72b6d783df94789baa560fbaef
namnium <anotherhollow1125@gmail.com>
2025-08-30 13:04:43 +0900

    fix: bindings 関連テスト・example の修正・強化

--- examples/examples/bindings.rs
+++ examples/examples/bindings.rs
@@ -2,14 +2,37 @@ use hooq::hooq;
 
 #[hooq]
 #[hooq::method(.inspect_err(|e| {
-    if *e > $threshold {
+    #[allow(clippy::nonminimal_bool)]
+    if *e > $threshold && !$skip {
         eprintln!("dif: {} with tag: {}", ($closure)(e, $threshold), $tag);
     }
 }))]
-#[hooq::tag = "threshold_check"]
+#[hooq::var(tag = "threshold_check")]
+#[hooq::binding(skip = false)]
 #[hooq::threshold = 5]
 #[hooq::closure = |e, t| e - t]
 fn func(val: usize) -> Result<(), usize> {
+    #[hooq::threshold = 9]
+    if val > 9 {
+        #[hooq::method(.inspect_err(|e| {
+            #[allow(clippy::nonminimal_bool)]
+            if *e > $threshold && !$skip {
+                eprintln!(
+                    "dif: {} with tag: {} with hoge: {}",
+                    ($closure)(e, $threshold),
+                    $tag,
+                    $hoge
+                );
+            }
+        }))]
+        #[hooq::hoge = "hoge"]
+        if val > 20 {
+            return Err(val);
+        }
+
+        return Err(val);
+    }
+
     Err(val)
 }
 

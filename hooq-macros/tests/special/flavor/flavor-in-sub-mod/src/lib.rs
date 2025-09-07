2ffe9738ff067f68aa11844df361590e8c7b2348
namnium <anotherhollow1125@gmail.com>
2025-09-08 05:11:52 +0900

    feat: lib.rs で toml_load!() を呼び出し、下位モジュールでも適用できることを確認

---
+++ hooq-macros/tests/special/flavor/flavor-in-sub-mod/src/lib.rs
@@ -0,0 +1,18 @@
+use hooq::toml_load;
+
+toml_load!();
+
+pub mod sub;
+
+#[test]
+fn test() {
+    use sub::{Checker, func_err, func_ok};
+
+    let mut checker = Checker::new();
+    func_ok(&mut checker).unwrap();
+    assert!(checker.checked());
+
+    let mut checker = Checker::new();
+    func_err(&mut checker).unwrap_err();
+    assert!(checker.checked());
+}

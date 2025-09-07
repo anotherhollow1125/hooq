fc97fd2ec9bc941fbf409b5ea21c9b9b6a5e0920
namnium <anotherhollow1125@gmail.com>
2025-09-07 17:40:32 +0900

    feat: flavor 関連のテストを追加

---
+++ hooq-macros/tests/special/flavor/empty.rs
@@ -0,0 +1,22 @@
+use hooq_macros::hooq;
+
+#[hooq(flavor = "empty")]
+fn enresult<T>(val: T) -> Result<T, ()> {
+    Ok(val)
+}
+
+#[hooq(empty)]
+fn func(flag: bool) -> Result<(), ()> {
+    enresult(())?;
+
+    if flag {
+        return Ok(());
+    }
+
+    Err(())
+}
+
+#[test]
+fn test() {
+    func(false).unwrap_err();
+}

8167af47fbfd11e9936ecb97409c6b1101132bc6
namnium <anotherhollow1125@gmail.com>
2025-10-16 23:08:31 +0900

    fix: rustfmt がないというCIエラーになったので以下2点を修正
    - nightly-2025-06-27 -> nightly に変更
    - ついでに昨今のshai-hulud対策のトレンドに合わせ各アクションのバージョンをSHA固定にした

--- hooq-macros/tests/special/flavor_not_cargo_project/build_script.rs
+++ hooq-macros/tests/special/flavor_not_cargo_project/build_script.rs
@@ -59,7 +59,7 @@ impl std::fmt::Display for BuildProfile {
 
 const ORIGINAL_HOOQ_PATH: &str = "../../../../../hooq";
 const SRC_NAME: &str = "main";
-const TOOLCHAIN: &str = "nightly-2025-06-27";
+const TOOLCHAIN: &str = "nightly";
 
 #[hooq(anyhow)]
 fn hooq_build(build_profile: BuildProfile) -> Result<()> {

9325ec9169a3198fdcab075e73c83b9f7924b3b9
namnium <anotherhollow1125@gmail.com>
2025-11-02 13:25:47 +0900

    fix: 追加した example をテストのものに合わせた

---
+++ examples/examples/consume_question.rs
@@ -0,0 +1,28 @@
+use hooq::hooq;
+
+fn enresult<T>(val: T) -> Result<T, String> {
+    Ok(val)
+}
+
+#[hooq]
+#[hooq::method(.unwrap()!)]
+pub fn func_unwrap() {
+    enresult(())?;
+}
+
+#[hooq]
+#[hooq::method(match $expr {
+    Ok(v) => v,
+    Err(e) => return Err(e),
+}!)]
+#[hooq::hook_targets("?")]
+#[allow(clippy::question_mark)]
+pub fn func_match() -> Result<(), String> {
+    enresult(())?;
+
+    Ok(())
+}
+fn main() {
+    func_unwrap();
+    func_match().unwrap();
+}

9e60871c2c81dd8355464983cae8edf056d878e0
namnium <anotherhollow1125@gmail.com>
2025-11-25 23:12:04 +0900

    fix: logのスナップショットテストがおかしい問題を修正

--- mdbook-source-code/flavor-log/src/main.rs
+++ mdbook-source-code/flavor-log/src/main.rs
@@ -2,7 +2,10 @@ use hooq::hooq;
 
 #[hooq(log)]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    env_logger::init();
+    env_logger::Builder::new()
+        .format_timestamp(None)
+        .filter_level(log::LevelFilter::Error)
+        .init();
 
     Err("Hello, world!".into())
 }

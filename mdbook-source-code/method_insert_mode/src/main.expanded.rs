89692e4d5bd84f87239693adb568d76ee635f4ee
namnium <anotherhollow1125@gmail.com>
2026-01-26 18:23:28 +0900

    fix: スナップショット更新

--- mdbook-source-code/method_insert_mode/src/main.expanded.rs
+++ mdbook-source-code/method_insert_mode/src/main.expanded.rs
@@ -3,15 +3,15 @@
 extern crate std;
 #[prelude_import]
 use std::prelude::rust_2024::*;
-
 use hooq::hooq;
 fn fallible<T>(val: T) -> Result<T, String> {
     Ok(val)
 }
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    fallible(()).inspect_err(|_| {
-        let _ = "inserted mode";
-    })?;
+    fallible(())
+        .inspect_err(|_| {
+            let _ = "inserted mode";
+        })?;
     fallible(())
         .inspect_err(|_| {
             let _ = "before chained";
